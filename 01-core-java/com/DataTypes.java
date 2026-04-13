df0a9a202de526e6123c862996fd286baa78f5e6
VarmaPothuri09 <nagasaivarmapothuri@gmail.com>
2026-04-13 12:59:46 +0530

    Add DataTypes Java files to core java

---
+++ 01-core-java/com/DataTypes.java
@@ -0,0 +1,31 @@
+package com;
+
+
+	public class DataTypes {
+		public static void main(String[] args) {
+			
+			byte temparature=40;
+			short villagePopulation=1450;
+			int carPrice=10000000;
+			long customerId=1234567898765443333L;
+			float distance=15.6473838474F;
+			double pi=3.143227599677767676868686868;
+			char alphabet='P';
+			boolean canSwim=false;
+			
+			
+			System.out.println(temparature);
+			System.out.println(villagePopulation);
+			System.out.println(carPrice);
+			System.out.println(customerId);
+			System.out.println(distance);
+			System.out.println(pi);
+			System.out.println(alphabet);
+			System.out.println(canSwim);
+			
+		}
+		}
+
+
+
+
